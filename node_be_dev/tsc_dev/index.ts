5368d3619036a5c63da0fcb13b6f271df2af79eb
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2022-12-03 23:55:51 +0100

    Correctly use the generics construct

--- node_be_dev/tsc_dev/index.ts
+++ node_be_dev/tsc_dev/index.ts
@@ -6,9 +6,9 @@ const arr1 = [2, 3, 4];
 const arr2 = ['one', 'two', 'three'];
 
 const one = getItem(arr1);
-const one_generic = <number>getItem(arr1);
+const one_generic = getItem<number>(arr1);
 const two = getItem(arr2);
-const two_generic = <string>getItem(arr2);
+const two_generic = getItem<string>(arr2);
 
 console.log(one, typeof one);
 console.log(one_generic, typeof one_generic);
