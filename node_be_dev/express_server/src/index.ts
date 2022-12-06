80d2d5cd4d67c6ac54aa0b06c529eb8114f2e734
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2022-12-06 15:55:51 +0100

    Define and use routes

--- node_be_dev/express_server/src/index.ts
+++ node_be_dev/express_server/src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 
+import routes from './routes/index';
+
 const app = express();
 const port = 3000;
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Server started!');
 });
+
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}!`);
